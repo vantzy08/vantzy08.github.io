6dc55bdaa69abe5d1b7a3cc6418eaaad6a22f39f
vantzy08 <irza.twin@gmail.com>
2025-03-13 13:13:49 +0700

    Add files via upload

---
+++ BunnyHug/index.js
@@ -0,0 +1,3 @@
+document.getElementById('nextPageButton').addEventListener('click', function() {
+    window.location.href = 'hug-bunny-game.html'; // Ganti dengan nama file halaman selanjutnya
+});
