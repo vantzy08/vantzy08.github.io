a500b9be0e99dc180733a5859d04d870d7366a99
vantzy08 <irza.twin@gmail.com>
2025-03-13 13:19:26 +0700

    Delete BunnyHug directory

--- BunnyHug/index.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('nextPageButton').addEventListener('click', function() {
-    window.location.href = 'hug-bunny-game.html'; // Ganti dengan nama file halaman selanjutnya
-});
